9decfb2d92fe49a4819bd5c3adb521cf9a002727
Brian Lambert <brianlambert2408@gmail.com>
2022-10-12 14:22:07 +1100

    Completed tasks

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,9 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{
+        projects.map((e)=><ProjectItem key={e.name} name={e.name} about= {e.about} technologies={e.technologies}/>)
+      }</div>
     </div>
   );
 }
